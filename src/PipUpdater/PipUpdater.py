9ad9e3daa3a11c062d65ea2586a3845226e5fb54
Alphons <93847055+Crazypersonalph@users.noreply.github.com>
2022-04-23 13:52:20 +0800

    Changed stuff

--- src/PipUpdater/PipUpdater.py
+++ src/PipUpdater/PipUpdater.py
@@ -5,11 +5,12 @@ amount_of_packages = packages.stdout.count("\n") - 3
 print("You have {} packages installed.".format(amount_of_packages))
 print("This can only be used to upgrade all of your packages at once.")
 print("I would not recommend using this tool if you have anything mission critical, otherwise, some programs may break")
+
 def input_warning():
     warning = input("Are you sure you want to upgrade all of your packages? (y/n)")
     if warning == "y":
         # Need to actually make it upgrade packages
-        print("WIP")
+        print(" ".join(packages.stdout.split()))
 
     elif warning == "n":
         print("Exiting...")
