525dc95e96eea4257ec0ecd0563efe8569367410
Alphons <93847055+Crazypersonalph@users.noreply.github.com>
2022-04-23 12:57:18 +0800

    Added the files

---
+++ src/PipUpdater/PipUpdater.py
@@ -0,0 +1,22 @@
+import subprocess
+
+packages = subprocess.run(["pip", "list", "command"], capture_output=True, text=True)
+amount_of_packages = packages.stdout.count("\n") - 3
+print("You have {} packages installed.".format(amount_of_packages))
+print("This can only be used to upgrade all of your packages at once.")
+print("I would not recommend using this tool if you have anything mission critical, otherwise, some programs may break")
+def input_warning():
+    warning = input("Are you sure you want to upgrade all of your packages? (y/n)")
+    if warning == "y":
+        # Need to actually make it upgrade packages
+        print("WIP")
+
+    elif warning == "n":
+        print("Exiting...")
+        exit()
+
+    else:
+        print("Please enter a valid input.\n")
+        return input_warning()
+
+input_warning()
